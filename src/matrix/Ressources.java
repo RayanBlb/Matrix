a84de22128867463e10d9ca53527d420ff3fdf4c
Antoine Poidevin <antoine.poidevin@teachonmars.com>
2022-04-22 10:51:20 +0200

    Antoine - finalisation constructeur

--- src/matrix/Ressources.java
+++ src/matrix/Ressources.java
@@ -1,11 +1,30 @@
 package matrix;
 
 public class Ressources {
-	
+
 	private int poids;
-	
-	public Ressources (int poids) {
-		this.poids=poids
+	private String id;
+
+	public Ressources(int poids, String id) {
+		this.poids = poids;
+		this.id = id;
+		
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public int getPoids() {
+		return poids;
+	}
+
+	public void setPoids(int poids) {
+		this.poids = poids;
 	}
 
 }
